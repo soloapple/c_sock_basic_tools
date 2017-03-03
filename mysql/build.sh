6d2bb99bdbc1171c15f7a139a450f6e766f2543c
soloapple <491870666@qq.com>
2017-03-03 19:00:35 +0000

    mysql and tags

--- mysql/build.sh
+++ mysql/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm -rf cccc
-gcc mysql_insert_table.c -L/lib64 -lmysqlclient -o cccc 
-scp cccc 172.24.141.28:/root
+#gcc mysql_insert_table.c -L/lib64 -lmysqlclient -o cccc 
+gcc mysql_query_table.c -L/lib64 -lmysqlclient -o cccc 
+scp cccc 172.24.141.27:/root
 rm -rf cccc
--- mysql/mysql_query_table.c
+++ mysql/mysql_query_table.c
@@ -24,41 +24,43 @@ int main(int argc, char *argv[])
 	char execsql[256];
 	int f1,f2,num_row,num_col;
 
-	if(mysql_init(&mysql_conn)!=NULL)
-	{
-		if(mysql_real_connect(&mysql_conn, "172.24.141.28", "root", "123456", "gap", MYSQL_PORT, "/tmp/mysql.sock", 0)!=NULL)
-		{
-			printf("connect success!!!\n");
-			sprintf(execsql,"select * from tab_system;");
-
-			if(mysql_query(&mysql_conn,execsql)==0)
-			{
-				printf("query success!\n");
-				mysql_result=mysql_store_result(&mysql_conn);
-				num_row=mysql_num_rows(mysql_result);
-				num_col=mysql_num_fields(mysql_result);
-				printf("row: %d,col: %d\n",num_row,num_col);
-				for(f1=0;f1<num_row;f1++)
-				{
-					mysql_row=mysql_fetch_row(mysql_result);
-					for(f2=0;f2<num_col;f2++)
-					{
-						printf("[Row %d,Col %d]==>[%s]\n",f1,f2,mysql_row[f2]);
-					}
-				}
-			}else
-			{
-				printf("query error!!!\n");
-			} 
-		}else
-		{
-			printf("connect failed\n");
-		}
-	}else
+	if ( mysql_init(&mysql_conn) == NULL )
 	{
 		printf("initial failed\n");
+		return 0;
 	}
+	
+	if ( mysql_real_connect(&mysql_conn, "127.0.0.1", "root", "123456", "gap", MYSQL_PORT, "/tmp/mysql.sock", CLIENT_MULTI_RESULTS) == NULL )
+	{
+		printf("%s\n", (const char*)mysql_error(&mysql_conn));
+		return 0;
+	}
+			
+	sprintf(execsql, "select msg, optime, reptime from tab_alarm;");
+
+	if (mysql_query(&mysql_conn,execsql)!=0)
+	{
+		printf("query error!!!\n");
+		return 0;
+	}
+		
+	printf("query success!\n");
+	mysql_result = mysql_store_result(&mysql_conn);
+	num_row = mysql_num_rows(mysql_result);
+	num_col = mysql_num_fields(mysql_result);
+
+	printf("row: %d,col: %d\n",num_row,num_col);
+	for ( f1 = 0; f1 < num_row; f1++ )
+	{
+		mysql_row = mysql_fetch_row(mysql_result);
+		for ( f2 = 0; f2 < num_col; f2++ )
+		{
+			printf("[Row %d,Col %d]==>[%s]\n",f1,f2,mysql_row[f2]);
+		}
+	}
+	
 	mysql_free_result(mysql_result);
 	mysql_close(&mysql_conn);
+
 	return 0;
 }
