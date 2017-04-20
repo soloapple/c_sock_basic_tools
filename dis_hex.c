9d52d19198354070391d75ad53ac9b963fe85f93
soloapple <491870666@qq.com>
2017-04-20 10:05:08 +0000

    modify dis_hex.c

--- dis_hex.c
+++ dis_hex.c
@@ -13,13 +13,33 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <getopt.h>
+
+char *file = NULL;
+char *pstr = NULL;
+
+char *const short_options = "f:s:h";
+struct option long_options[] =
+{
+	{"file", 1, NULL, 'f'},      
+	{"str", 1, NULL, 's'},      
+	{"help", 0, NULL, 'h'},      
+	{0, 0, 0, 0},
+};
+
 void
 prt_char(char c)
 {
 	if (isprint(c))
 		printf("%c", c);
 	else
-		printf(" ");
+		printf("*");
 }
 
 void
@@ -28,7 +48,7 @@ prt_hex(char c)
 	if (isprint(c))
 		printf(" %02x ", c);
 	else
-		printf(" %02x ", c);
+		printf(" ** ");
 }
 
 int
@@ -48,6 +68,11 @@ print_hex(char *buf, int len)
 	int line_num;
 	int time = 16;
 	int x;
+	int n_polish;
+	bool show_mid = false;
+
+	int last_line = get_line_head_num(len);
+	int last_head = last_line * 16;
 
 	for ( i=0; i<len; i++ )
 	{
@@ -61,8 +86,7 @@ print_hex(char *buf, int len)
 
 		if ( (((i+1)%16) == 0) )
 		{
-			line_num = get_line_head_num(i);
-			j = 16 * line_num;
+			j = get_line_head_num(i) * 16;
 
 			printf("     ");
 			for( x=0; x<16; x++, j++ )
@@ -72,17 +96,121 @@ print_hex(char *buf, int len)
 			puts("");
 		}
 	}
+
+	/* print last line hex. */
+	n_polish = len;
+	if ( n_polish < 8 )	
+		show_mid = true;
+
+	while ( (n_polish % 16) != 0 )
+	{
+		if ( (((n_polish)%8) == 0) && show_mid )
+			printf(" -- ");
+
+		printf(" ** ");
+		n_polish++;
+	}
+
+	/* print last line str. */
+	j = last_head;
+	printf("     ");
+	for( x=0; x<16; x++, j++ )
+	{
+		prt_char(buf[j]);
+	}
+	puts("");
 }
 
+int 
+read_conf(char *file, char **p, size_t fsize)
+{
+	FILE *fp = fopen(file, "r+");
+	if ( fp == NULL )
+	{
+		printf ( "file is null\n" );
+		return -1;
+	}
+
+	int n = fread(*p, 1, fsize, fp);
+
+	return n;
+}
+
+
+/* 
+ * Name:  usage
+ * Description:  
+ */ 
+void
+usage(int argc, char *argv[]) 
+{
+	printf( 
+			"USAGE: exec -f filename|-s string.\n\n"
+			" h, --help	display the help and exit.\n"
+			" f, filename, file to display to hex.\n"
+			" s, string to display.\n"
+	);
+}    
+
+void
+parse_arg(int argc, char *argv[])
+{
+	int c;      
+	while((c = getopt_long (argc, argv, short_options, long_options, NULL)) != -1)      
+	{
+		switch (c)
+		{
+			case 'f':
+				file = strdup(optarg);
+				break;
+			
+			case 's':
+				pstr = strdup(optarg);
+				break;
+			
+			case 'h':
+				usage(argc, argv);	
+				break;
+		
+			default:
+				usage(argc, argv);	
+				break;
+		}
+	}
+}
+
+size_t
+get_file_size(char *path)  
+{  
+	size_t filesize = -1;      
+	struct stat statbuff;  
+	if(stat(path, &statbuff) < 0){  
+		return filesize;  
+	}else{  
+		filesize = statbuff.st_size;  
+	}  
+	return filesize;  
+}  
 
 int
 main(int argc, char *argv[])
 {
-	if ( argc != 2 )
-		printf("USAGE: %s string", argv[0]);
+	parse_arg(argc, argv);
 
-	char *s = argv[1];
-	print_hex(s, 48);
+	if (file != NULL)
+	{
+		size_t fsize = get_file_size(file);
+		char *p = (char*)calloc(fsize, 1);
+		read_conf(file, &p, fsize);
+		print_hex(p, fsize);
+
+		free(p);
+	}
+
+	if (pstr != NULL)
+	{
+		print_hex(pstr, strlen(pstr));
+	}
 	
 	return 0;
 }
