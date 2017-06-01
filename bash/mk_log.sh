c3b24620c32ab4ebea872d1abb4594ba095706fb
soloapple <491870666@qq.com>
2017-06-01 02:51:51 +0000

    FILE : mk_log.sh REASON: self make logdata

---
+++ bash/mk_log.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for i in {1..100000}
+do
+	task_id=${i}
+	logger -t "filesync" -p local4.info "mode=\"10\", type=\"6\", msg=\"应用:内置模式, 操作:新增任务, 任务号:${task_id}\""
+	logger -t "manage" -p local1.info "mode=\"50\",  msg=\"管理日志\""
+done
