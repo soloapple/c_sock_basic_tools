45ef718392c39e5b4d07dcc594880012e63fc536
soloapple <491870666@qq.com>
2017-06-03 12:40:07 +0000

    FILE: mk_log.sh REASON: change some log format

--- bash/mk_log.sh
+++ bash/mk_log.sh
@@ -2,7 +2,12 @@
 
 for i in {1..100000}
 do
+	x=$(($i%10000))
+	if [ $x -eq 0 ];then
+		echo $i
+	fi
+
 	task_id=${i}
 	logger -t "filesync" -p local4.info "mode=\"10\", type=\"6\", msg=\"应用:内置模式, 操作:新增任务, 任务号:${task_id}\""
-	logger -t "manage" -p local1.info "mode=\"50\",  msg=\"管理日志\""
+	logger -t "manage" -p local1.info "mode=\"50\",  msg=\"管理日志:${task_id}\""
 done
